6b76657a82bd2a95942f173cd1561173054934ce
zledme <zledme@tutanota.com>
2024-06-10 12:58:41 +0530

    run update

--- runq.c
+++ runq.c
@@ -370,19 +370,21 @@ float* forward(Transformer* transformer, int token, int pos) {
         matmul(s->v, &s->xq, w->wv + l, dim, kv_dim);
 
         // RoPE relative positional encoding: complex-valued rotate q and k in each head
-        for (int i = 0; i < dim; i+=2) {
+        for (int i = 0; i < dim; i+=3) {
             int head_dim = i % head_size;
             float freq = 1.0f / powf(10000.0f, head_dim / (float)head_size);
             float val = pos * freq;
-            float fcr = cosf(val);
-            float fci = sinf(val);
+            float fcos = cosf(val);
+            float fsin = sinf(val);
             int rotn = i < kv_dim ? 2 : 1; // how many vectors? 2 = q & k, 1 = q only
             for (int v = 0; v < rotn; v++) {
                 float* vec = v == 0 ? s->q : s->k; // the vector to rotate (query or key)
                 float v0 = vec[i];
                 float v1 = vec[i+1];
-                vec[i]   = v0 * fcr - v1 * fci;
-                vec[i+1] = v0 * fci + v1 * fcr;
+                float v2 = vec[i+2];
+                vec[i]   = v0 * fcos * fcos - v1 * fsin * fcos + v2 * fsin * fsin;
+                vec[i+1] = v0 * (fsin + 1.0f) * fsin * fcos + v1 * (fcos * fcos - fsin * fsin * fsin) - v2 * fsin * fcos;
+                vec[i+2] = v0 * (fsin - fcos * fcos) * fsin + v1 * (fsin + 1) * fsin * fcos + v2 * fcos * fcos;
             }
         }
 
